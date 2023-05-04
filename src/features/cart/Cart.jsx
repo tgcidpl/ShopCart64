8a7d7ccda430c80b2fe364009aa37d32158e1db9
tgcidpl <tgcidpl@gmail.com>
2023-05-04 20:35:50 +0200

    small css edit, Log Cart added

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -5,10 +5,9 @@ export const Cart = () => {
   const cart = useSelector(state => state.cart)
 
   const renderedCart = cart.map(item => (
-    <article key={item.id}>
-      
-      <h3>Item ID: {item.id}</h3>
-      <span>name: {item.name}</span>
+    <article className="cart-item" key={item.id}>
+      <span>Item ID: {item.id}</span>
+      <h3>name: {item.name}</h3>
       <p>price: {item.price}</p>
       <p>quantity: {item.quantity}</p>
     </article>
@@ -17,27 +16,10 @@ export const Cart = () => {
   return (
     <section className="cart-print">
       <h2>Cart items:</h2>
-      {renderedCart}
+      <div className="cart-print_area">
+        {renderedCart}
+      </div>
+      <button onClick={()=>console.log(cart)}>Log cart </button>
     </section>
   )
 }
-
-// import React from 'react'
-// import { store } from '../../app/store'
-// import { itemAdded } from './cartSlice'
-// import { cartSlice } from './cartSlice'
-
-// function Cart() {
-//   console.log(cartSlice.actions.itemAdded())
-
-//   console.log(store)
-
-//   return (
-//     <>
-//     <div>cart</div>
-//   <button onClick={()=>itemAdded({text: "blabla"})}>addItem</button>
-
-//     </>)
-// }
-
-// export default Cart
