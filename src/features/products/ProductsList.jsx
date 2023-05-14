c875e1718762c2ed0ebf824e94c73d55a20a3c70
tgcidpl <tgcidpl@gmail.com>
2023-05-14 22:27:16 +0200

    new Cart WIP so it works with new inventory

--- src/features/products/ProductsList.jsx
+++ src/features/products/ProductsList.jsx
@@ -1,34 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addedToCart } from "../cart/cartSlice";
 import { Products } from "./Products";
 
 export const ProductsList = () => {
   const dispatch = useDispatch();
+  const [renderedItemList, setRenderedItemList] = useState(null);
 
-  console.log(Products());
-  const itemList = [];
-  const renderedItemList = itemList.map((item) => (
-    <article className="itemList-item" key={item.id}>
-      <p>Item ID: {item.id}</p>
-      <h4>Product name: {item.name}</h4>
-      <p>Price: ${item.price}</p>
-      <p>Quantity: {item.quantity}</p>
-      <button
-        onClick={() =>
-          dispatch(
-            addedToCart({
-              id: item.id,
-              name: item.name,
-              price: item.price,
-            })
-          )
-        }
-      >
-        Add to Cart
-      </button>
-    </article>
-  ));
+  useEffect(() => {
+    Products().then((productsArray) => {
+      const itemList = productsArray.map((item) => (
+        <article className="itemList-item" key={item.id}>
+          <p>Item ID: {item.id}</p>
+          <h4>Product name: {item.name}</h4>
+          <p>Price: ${item.price}</p>
+          <p>Quantity: {item.quantity}</p>
+          <button
+            onClick={() =>
+              dispatch(
+                addedToCart({
+                  id: item.id,
+                  name: item.name,
+                  price: item.price,
+                  quantity: 1,
+                  maxQuantity: item.quantity,
+                })
+              )
+            }
+          >
+            Add to Cart
+          </button>
+        </article>
+      ));
+      setRenderedItemList(itemList);
+    });
+  }, [dispatch]);
+
+  if (!renderedItemList) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <section className="itemList-print">
