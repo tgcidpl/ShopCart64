ce3bf82fb41e5c3028b58aa9e2fa79d190cb3ff9
tgcidpl <tgcidpl@gmail.com>
2023-05-17 17:12:01 +0200

    deleted previous itemList components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import "./App.scss";
-import { AddToItemList } from "./features/addToItemList/AddToItemList";
 import { Cart } from "./features/cart/cart";
-import { ItemList } from "./features/itemList/ItemList";
 import { ProductsList } from "./features/products/ProductsList";
 
 function App() {
   return (
     <>
       <ProductsList />
-      {/* <ItemList /> */}
-      {/* below component is only used to temporarily add new items to ItemList */}
-      {/* <AddToItemList /> */}
       <Cart />
     </>
   );
--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import itemListReducer from '../features/itemList/itemListSlice'
 import cartReducer from "../features/cart/cartSlice";
 
 export const store = configureStore({
   reducer: {
-    itemList: itemListReducer,
-    cart: cartReducer
-  }
-})
+    cart: cartReducer,
+  },
+});
